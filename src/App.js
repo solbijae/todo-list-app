653bbbed7c7c1356189eae1701a1d1fae9e1f9be
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-03-06 17:03:33 +0900

    Added textfield

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+
+const TodoItemInputField = (props) => {
+  const [input, setInput] = useState("");
+
+  return (<div>
+    <TextField
+      id="todo-item-input"
+      label="Todo item"
+      variant="outlined" 
+      value={input} 
+      onChange = {(e) => setInput(e.target.value)}
+    />
+  </div>);
+};
+  
 
 function App() {
   return (
     <div className="App">
-
+      <TodoItemInputField />
     </div>
   );
 }
